78bd10dccc1efba0ca65a8dc6423d235d8d33199
ok <ok@ok>
2024-12-25 00:07:19 -0500

    merge num nav

--- keymap.c
+++ keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├──────┴───────────┼───────────┼───────────┼───────────┼────┼───────┼────┼───────────┼───────────┼───────────┼─────────────────┼─────────────────────┼──────┼──────────┘
 //    │        '         │     z     │     x     │     c     │ v  │   b   │ no │     n     │     m     │     -     │        =        │          /          │  up  │
 //    ├──────┬───────────┼───────────┼───────────┴───────────┴────┴───────┴────┴───────────┴───────────┼───────────┼─────────────────┼─────────────┬───────┼──────┼──────────┐
-//    │ lctl │  MO(SYM)  │  MO(NUM)  │                          LT(NAV, spc)                           │   lsft    │ LT(MOUSE, bspc) │ LT(FN, del) │ left  │ down │   rght   │
+//    │ lctl │  MO(NUM)  │  MO(SYM)  │                          LT(NAV, spc)                           │   lsft    │ LT(MOUSE, bspc) │ LT(FN, del) │ left  │ down │   rght   │
 //    └──────┴───────────┴───────────┴─────────────────────────────────────────────────────────────────┴───────────┴─────────────────┴─────────────┴───────┴──────┴──────────┘
 [BASEMOD] = LAYOUT_ansi_82(
   KC_ESC     , KC_F1        , KC_F2        , KC_F3        , KC_F4        , KC_F5 , KC_F6 , KC_F7  , KC_F8        , KC_F9        , KC_F10       , KC_F11             , KC_F12         , TG(5)   ,           DF(BASE),
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   S(KC_COMM) , KC_Q         , KC_W         , KC_E         , KC_R         , KC_T  , TD(0) , KC_Y   , KC_U         , KC_I         , KC_O         , KC_P               , KC_P           , KC_BSLS ,           TG(3)   ,
   KC_F13     , LCTL_T(KC_A) , LSFT_T(KC_S) , LGUI_T(KC_D) , LALT_T(KC_F) , KC_G  , KC_NO , KC_H   , RALT_T(KC_J) , RGUI_T(KC_K) , RSFT_T(KC_L) , RCTL_T(KC_SCLN)    ,                   KC_ENT ,           TG(4)   ,
   KC_QUOT                   , KC_Z         , KC_X         , KC_C         , KC_V  , KC_B  , KC_NO  , KC_N         , KC_M         , KC_MINS      , KC_EQL             ,                  KC_SLSH , KC_UP             ,
-  KC_LCTL    , MO(SYM)      , MO(NUM)      ,                                  LT(NAV, KC_SPC)                                   , KC_LSFT      , LT(MOUSE, KC_BSPC) , LT(FN, KC_DEL) , KC_LEFT , KC_DOWN , KC_RGHT
+  KC_LCTL    , MO(NUM)      , MO(SYM)      ,                                  LT(NAV, KC_SPC)                                   , KC_LSFT      , LT(MOUSE, KC_BSPC) , LT(FN, KC_DEL) , KC_LEFT , KC_DOWN , KC_RGHT
 ),
 
 //    ┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┬─────────┬─────────┬─────────┬─────────┬────────────┬─────────┬─────────┬──────┐      ┌───────┐
